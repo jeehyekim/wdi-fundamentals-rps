e3238d5f26028a037ea806c6bddb93ba794f91b8
Jeehye Kim <jeehyekim@Jeehyes-MacBook-Pro.local>
2015-09-18 17:53:59 -0700

    revised function getWinner()

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
+/*'use strict'; */
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
@@ -31,10 +31,13 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove === computerMove) {
+        winner = "tie";
+    } else if (playerMove == "scissors" && computerMove == "paper" || playerMove == "rock" && computerMove == "scissors" || playerMove == "paper" && computerMove == "rock") {
+        winner = "player";
+    } else {
+        winner = "computer";
+    }
     return winner;
 }
 

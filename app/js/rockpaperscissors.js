ed17fe3128cc30caa761f4597d68f377692a17c1
Jeehye Kim <jeehyekim@Jeehyes-MacBook-Pro.local>
2015-09-22 13:33:49 -0700

    completed 'rock', 'paper', 'scissors' game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -45,8 +45,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+ 
+var winner;
+while(playerWins < 5 && computerWins < 5){
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    winner = getWinner (playerMove, computerMove);
+    if (winner === 'player') {
+        playerWins++;
+    } else if (winner === 'computer') {
+        computerWins++;
+    } else playerWins += 0;
+}
     return [playerWins, computerWins];
 }
 
+
+playToFive();
